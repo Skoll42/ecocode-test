b11acc7ee3db93fa96c2a608f4e64d1c2ec65544
Skoll <akostko@gmail.com>
2019-06-07 04:15:09 +0300

    added pagination for movies view as it is planned to crawl huge movie db

--- config/bundles.php
+++ config/bundles.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-    // 'all' means that the bundle is enabled for any Symfony environment
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
@@ -11,10 +10,10 @@ return [
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-    // these bundles are enabled only in 'dev' and 'test', so you can't use them in 'prod'
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -6,18 +6,32 @@ use App\Entity\Movie;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+// Include paginator interface
+use Knp\Component\Pager\PaginatorInterface;
 
 class MovieController extends AbstractController
 {
-
     /**
+     *
      * @Route("/movies", name="app_movie_list")
+     * @param Request $request
+     * @param PaginatorInterface $paginator
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index()
+    public function index(Request $request, PaginatorInterface $paginator)
     {
         $movieRepository = $this->getDoctrine()->getRepository(Movie::class);
 
-        return $this->render('movie-list.html.twig', ['movies' => $movieRepository->findAll()]);
+        $allMoviesQuery = $movieRepository->createQueryBuilder('p')->getQuery();
+
+        $movies = $paginator->paginate(
+            $allMoviesQuery,
+            $request->query->getInt('page', 1),
+            20
+        );
+
+        return $this->render('movie-list.html.twig', ['movies' => $movies]);
     }
 }
