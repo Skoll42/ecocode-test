b702d2326432c7496a45bca65bcb9889fdcd7a59
Skoll <akostko@gmail.com>
2019-06-07 04:04:12 +0300

    Locale and UserLocale event subscribers

---
+++ src/EventSubscriber/LocaleSubscriber.php
@@ -0,0 +1,57 @@
+<?php
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+
+class LocaleSubscriber implements EventSubscriberInterface
+{
+    private $defaultLocale;
+
+    public function __construct($defaultLocale = 'en')
+    {
+        $this->defaultLocale = $defaultLocale;
+    }
+
+    public function onKernelRequest(RequestEvent $event)
+    {
+        $request = $event->getRequest();
+        if (!$request->hasPreviousSession()) {
+            return;
+        }
+
+        // try to see if the locale has been set as a _locale routing parameter
+        if ($locale = $request->attributes->get('_locale')) {
+            $request->getSession()->set('_locale', $locale);
+        } else {
+            // if no explicit locale has been set on this request, use one from the session
+            $request->setLocale($request->getSession()->get('_locale', $this->defaultLocale));
+        }
+    }
+
+    public function setLocaleForUnauthenticatedUser(RequestEvent $event)
+    {
+        if (HttpKernelInterface::MASTER_REQUEST !== $event->getRequestType()) {
+            return;
+        }
+
+        $request = $event->getRequest();
+
+        if ('undefined' == $request->getLocale()) {
+            $request->setLocale($request->getPreferredLanguage());
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            // must be registered before (i.e. with a higher priority than) the default Locale listener
+            KernelEvents::REQUEST => [
+                ['onKernelRequest', 20],
+                ['setLocaleForUnauthenticatedUser', 0]
+            ],
+        ];
+    }
+}
